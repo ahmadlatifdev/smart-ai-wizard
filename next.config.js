2a7c5e83083da808b62011cedd9127b712557551
ahmadlatifdev <privategate777@gmail.com>
2025-04-21 19:40:57 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  compiler: {
+    reactRemoveProperties: true,
+  },
+  experimental: {
+    reactRoot: true,
+  },
+  webpack(config) {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  }
+}
+
 module.exports = nextConfig;
